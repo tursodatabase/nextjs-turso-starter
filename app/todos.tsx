f29f68231b8ac4768cfaca776c80bc195365528f
Jamie Barton <jamie@notrab.dev>
2025-06-06 11:25:36 +0100

    migrate on build

--- app/todos.tsx
+++ app/todos.tsx
@@ -1,15 +1,8 @@
-// import { migrate } from "drizzle-orm/libsql/migrator";
-
-// import { db } from "@/db";
 import { getTodos } from "@/app/actions";
 import { TodoList } from "./todo-list";
 
 export async function Todos() {
   try {
-    // Create the todos table if it doesn't exist
-    // await migrate(db, { migrationsFolder: "./migrations" });
-
-    // Get todos using our new raw SQL function
     const todos = await getTodos();
 
     return <TodoList initialTodos={todos || []} />;
